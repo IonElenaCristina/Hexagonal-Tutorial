af61fa62a11e3de2539f431cf58046a73b3cbe5c
Ion Elena Cristina <ionelenacristina0@gmail.com>
2023-11-27 21:58:50 +0200

    d

--- shortener/model.go
+++ shortener/model.go
@@ -2,6 +2,6 @@ package shortener
 
 type Redirect struct {
 	Code      string `json:"code" bson:"code"` // I have to goes bson for mongoDB
-	URL       string `json:"url" bson:"url"`
+	url       string `json:"url" bson:"url"`
 	CreatedAt int64  `json:"createdAt" bson:"createdAt"`
 }
